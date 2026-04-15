0fbe60003ba6bce1252aec05537df445e3d15cf6
vaishali86c <vaishali86c@gmail.com>
2026-04-16 00:26:38 +0530

    Add steps for user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,12 +2,31 @@ import  { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
-    console.log(res.json())
+    // get user details from frontend
+
+    // validation - not empty
+
+    // check if user already exists: username, email
+
+    // check for images, check for avatar
+
+    // upload to them cloudinary, avatar
+
+    // create user object - create entry in db
+
+    // remove password and refresh token feild from response
+
+    // check for user creation
+    
+    // return response if not please throw error
+
+    const {fullName, email, username, password } = req.body
+
+    console.log("email", email);
 })
 
 
 
-export { registerUser }
+export { 
+    registerUser,
+}
