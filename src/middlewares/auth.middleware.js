fe98b38fcbef5631805abab6e841d28d13aabc54
vaishali86c <vaishali86c@gmail.com>
2026-04-20 22:31:37 +0530

    Add refreshToken end point

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
     
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
@@ -16,7 +16,6 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if (!user) {
-            //  T000 discuss about frontend
             throw new ApiError(401, "Invalid Access Token")
         } 
     
