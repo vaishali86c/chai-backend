8483b8b1a2121d70921ec0fd5de095dd15047f4a
vaishali86c <vaishali86c@gmail.com>
2026-04-21 19:43:06 +0530

    Fix refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -166,13 +166,13 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async(req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }   
 
     try {
         const decodedToken = jwt.verify(
-            incomingRefreshAccessToken,
+            incomingRefreshToken,
             process.env.REFRESH_TOKEN_SECRET
         )
     
