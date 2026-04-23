fd249f9037016ebbb49b52e5e068f0f65ecff9cf
vaishali86c <vaishali86c@gmail.com>
2026-04-23 21:03:17 +0530

    Typo Fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,9 +36,9 @@ router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/changed-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.router("/update-account").patch(verifyJWT, updateAccountDetails) // used patch
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) // used patch
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
